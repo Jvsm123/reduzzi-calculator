e235ccf69bcfa5ed5a104e0f3c7d7b2e710cc309
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2024-05-01 15:09:55 -0300

    new features

---
+++ src/pages/Home/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-[var(--main-blue)] h-[200px] mt-20">
+      <h1>Footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AppBar from "./AppBar";
+import Footer from './Footer';
 
 const Home = () => {
   return (
@@ -16,52 +17,34 @@ const Home = () => {
           Instrução Normativa RFB Nº 2.021 de 16/04/2021
         </p>
 
-        <div className="mt-[60px]">
+        <div className="mt-[60px] mb-4">
           <h2 className="text-2xl font-bold">DADOS DO PROPRIETÁRIO</h2>
         </div>
 
-        <section className="mt-4 flex gap-14">
-          <div className="bg-[var(--bg-modal-whitegray)] rounded-[10px] p-6 w-full">
-            <div className="flex flex-col gap-2">
-              <label
-                htmlFor=""
-                className="text-[var(--gray)] text-xl font-semibold"
-              >
-                Responsável pela obra
-              </label>
-              <Input />
+        <section className="flex gap-14">
+          <div className="w-full">
+            <DadosDoProprietario />
+
+            <div className="mt-[60px] mb-4">
+              <h2 className="text-2xl font-bold">DADOS DA OBRA</h2>
             </div>
+            <DadosObra />
 
-            <section className="flex justify-between gap-10 mt-7">
-              <div className="w-full flex flex-col gap-2">
-                <label
-                  htmlFor=""
-                  className="text-[var(--gray)] text-xl font-semibold"
-                >
-                  DDD + Celular
-                </label>
-                <Input />
-              </div>
-
-              <div className="w-full flex flex-col gap-2">
-                <label
-                  htmlFor=""
-                  className="text-[var(--gray)] text-xl font-semibold"
-                >
-                  Tipo de proprietário
-                </label>
-                <Input />
-              </div>
-            </section>
+            <div className="mt-[60px] mb-4">
+              <h2 className="text-2xl font-bold">METRAGEM DA OBRA</h2>
+            </div>
+            <MetragemObra />
           </div>
 
           <div>
-            <button className="bg-[#00CC93] text-white text-xl font-semibold px-14 py-3 rounded-lg">CALCULAR</button>
+            <button className="bg-[#00CC93] text-white text-xl font-semibold px-14 py-3 rounded-lg">
+              CALCULAR
+            </button>
           </div>
         </section>
       </section>
 
-      <footer className="w-full bg-[var(--main-blue)] h-[200px] mt-20"></footer>
+      <Footer/>
     </main>
   );
 };
@@ -76,6 +59,132 @@ const Input = () => {
         placeholder="Preencha aqui..."
         className="focus:outline-none border-[2px] focus:border-[var(--green-input)] rounded-[8px] text-lg text-gray-400 focus:font-medium w-full p-2"
       />
+    </div>
+  );
+};
+
+const DadosDoProprietario = () => {
+  return (
+    <div className="bg-[var(--bg-modal-whitegray)] rounded-[10px] p-6 w-full">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="" className="text-[var(--gray)] text-xl font-semibold">
+          Responsável pela obra
+        </label>
+        <Input />
+      </div>
+
+      <section className="flex justify-between gap-10 mt-7">
+        <div className="w-full flex flex-col gap-2">
+          <label
+            htmlFor=""
+            className="text-[var(--gray)] text-xl font-semibold"
+          >
+            DDD + Celular
+          </label>
+          <Input />
+        </div>
+
+        <div className="w-full flex flex-col gap-2">
+          <label
+            htmlFor=""
+            className="text-[var(--gray)] text-xl font-semibold"
+          >
+            Tipo de proprietário
+          </label>
+          <Input />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const DadosObra = () => {
+  return (
+    <div className="bg-[var(--bg-modal-whitegray)] rounded-[10px] p-6 w-full flex justify-between gap-5 mt-7 flex-wrap">
+        <div className="w-full flex flex-col gap-2 max-w-[48%]">
+          <label
+            htmlFor=""
+            className="text-[var(--gray)] text-xl font-semibold"
+          >
+            Destinação da obra
+          </label>
+          <Input />
+        </div>
+
+        <div className="w-full flex flex-col gap-2 max-w-[48%]">
+          <label
+            htmlFor=""
+            className="text-[var(--gray)] text-xl font-semibold"
+          >
+            Obra com financiamento?
+          </label>
+          <Input />
+        </div>
+
+        <div className="w-full flex flex-col gap-2 max-w-[48%]">
+          <label
+            htmlFor=""
+            className="text-[var(--gray)] text-xl font-semibold"
+          >
+            UF da obra
+          </label>
+          <Input />
+        </div>
+
+        <div className="w-full flex flex-col gap-2 max-w-[48%]">
+          <label
+            htmlFor=""
+            className="text-[var(--gray)] text-xl font-semibold"
+          >
+            Cidade da obra
+          </label>
+          <Input />
+        </div>
+
+        <div className="w-full flex flex-col gap-2">
+          <label
+            htmlFor=""
+            className="text-[var(--gray)] text-xl font-semibold"
+          >
+            Fase atual da obra
+          </label>
+          <Input />
+        </div>
+    </div>
+  );
+};
+
+const MetragemObra = () => {
+  return (
+    <div className="bg-[var(--bg-modal-whitegray)] rounded-[10px] p-6 w-full flex justify-between gap-5 mt-7 flex-wrap">
+      <div className="w-full flex flex-col gap-2 max-w-[48%]">
+        <label htmlFor="" className="text-[var(--gray)] text-xl font-semibold">
+          M² DE CONSTRUÇÃO{" "}
+          <span className="text-sm italic">(EXCETO PISCINA OU QUADRA)</span>
+        </label>
+        <Input />
+      </div>
+
+      <div className="w-full flex flex-col gap-2 max-w-[48%]">
+        <label htmlFor="" className="text-[var(--gray)] text-xl font-semibold">
+          M² DE PISCINA + QUADRA POLIESPORTIVA
+        </label>
+        <Input />
+      </div>
+
+      <div className="w-full flex flex-col gap-2 max-w-[48%]">
+        <label htmlFor="" className="text-[var(--gray)] text-xl font-semibold">
+          INÍCIO DA CONSTRUÇÃO
+        </label>
+        <Input />
+      </div>
+
+      <div className="w-full flex flex-col gap-2 max-w-[48%]">
+        <label htmlFor="" className="text-[var(--gray)] text-xl font-semibold">
+          PREVISÃO DE TÉRMINO
+        </label>
+        <Input />
+      </div>
     </div>
   );
 };
