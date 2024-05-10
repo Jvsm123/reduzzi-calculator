182c57150a30cc7d9214c38ca31f02c048ca26a5
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2024-05-09 21:23:18 -0300

    create responsive ui result page

--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -8,7 +8,7 @@ import Footer from "../Home/Footer";
 import { WhatsappHandler } from "../../componentes/WhatsappHandler";
 
 const DivResultData = ({ label, value }) => (
-  <div className="w-full flex flex-col gap-2 max-w-[48%]">
+  <div className="w-full flex flex-col gap-2 md:max-w-[250rem]">
     <label className="text-xl font-semibold text-[var(--gray)]">{label}</label>
     <div className="border border-gray-300 rounded p-3">
       <div className="text-lg text-gray-500">{value}</div>
@@ -44,24 +44,33 @@ const ResultPage = () => {
           <h2 className="text-4xl font-bold">RESULTADO</h2>
         </div>
 
-        <section className="bg-[#ffffff] mb-20 mx-14 border border-[#006837ff] rounded-[8rem]">
-          <div className="bg-[var(--bg-gray-detail)] flex gap-10 px-10 py-7 rounded-t-lg">
+        <section className="bg-[#ffffff] mb-20 mx-7 md:mx-14 border border-[#006837ff] rounded-[8rem]">
+          <div className="bg-[var(--bg-gray-detail)] flex gap-10 px-10 py-7 rounded-t-lg flex-wrap justify-center">
             <DivResultData
               label="Área total"
-              value={`${Number(metroTotal).toLocaleString({ maximumFractionDigits: 2, minimumFractionDigits: 2 })} m²`}
+              value={`${Number(metroTotal).toLocaleString({
+                maximumFractionDigits: 2,
+                minimumFractionDigits: 2,
+              })} m²`}
             />
             <DivResultData
               label="Complementar"
-              value={`${Number(m2PiscinaQuadra).toLocaleString({ maximumFractionDigits: 2, minimumFractionDigits: 2 })} m²`}
+              value={`${Number(m2PiscinaQuadra).toLocaleString({
+                maximumFractionDigits: 2,
+                minimumFractionDigits: 2,
+              })} m²`}
             />
             <DivResultData
               label="RMT gerada"
-              value={`${Number(rmtObra).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`}
+              value={`${Number(rmtObra).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}`}
             />
             <DivResultData label="Tabela VAU" value={`${valorVau}`} />
           </div>
 
-          <div className="flex justify-center gap-12 mt-14">
+          <div className="flex justify-center gap-12 mt-14 flex-wrap">
             <div className="text-[#666666ff]">
               <p className="text-[30rem] font-medium ml-10">Na regra antiga</p>
               <span className="inline-flex items-end gap-3">
@@ -79,7 +88,10 @@ const ResultPage = () => {
               <span className="inline-flex items-end gap-3">
                 <p className="text-[24rem] font-medium italic mb-2">Por</p>
                 <p className="text-[#063958ff] border border-[#ccccccff] rounded-[8rem] inline-block px-4 py-2 text-4xl font-bold">
-                  {`${Math.round(Number(totalImpostoComReducao)).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`}
+                  {`${Math.round(Number(totalImpostoComReducao)).toLocaleString(
+                    "pt-BR",
+                    { style: "currency", currency: "BRL" }
+                  )}`}
                 </p>
               </span>
             </div>
@@ -107,19 +119,27 @@ const ResultPage = () => {
           </div>
           */}
 
-          <div className="bg-[#006837ff] flex justify-between rounded-b-lg overflow-hidden">
-            <section className="flex items-center justify-between w-[60%] py-6 px-10">
+          <div className="bg-[#006837ff] flex justify-between rounded-b-lg md:overflow-hidden flex-wrap lg:flex-nowrap">
+            <section className="flex md:items-center justify-between w-full lg:w-[60%] py-6 px-10 flex-col md:flex-row">
               <p className="border-l-[5rem] border-[#00cc93ff] text-white font-bold text-[24rem] pl-3">
                 ECONOMIA <br />
                 GERADA:
               </p>
 
-              <p className="text-white font-bold text-[50rem]">{`${Number(23000 - totalImpostoComReducao).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`}</p>
+              <p className="text-white font-bold text-[40rem] sm:text-[50rem]">{`${Number(
+                23000 - totalImpostoComReducao
+              ).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}`}</p>
             </section>
 
-            <section className="bg-[#063958ff] text-center py-3 px-10">
+            <section className="bg-[#063958ff] text-center py-3 px-10 w-full lg:max-w-[40%]">
               <p className="text-white font-bold text-[40rem]">
-                {`${(100 - (Number(totalImpostoComReducao) / 23000) * 100).toFixed(0)}% DE REDUÇÃO`}
+                {`${(
+                  100 -
+                  (Number(totalImpostoComReducao) / 23000) * 100
+                ).toFixed(0)}% DE REDUÇÃO`}
               </p>
               <p className="text-white font-bold text-[16rem] text-center">
                 BASE LEGAL ATUAL DA RECEITA FEDERAL CONFORME IN. 2021/2021
@@ -129,7 +149,7 @@ const ResultPage = () => {
         </section>
 
         {/* FORMA DE PAGAMENTO DOS IMPOSTOS */}
-        <section className="mx-14 border border-[#999999ff] rounded-[8rem] relative flex flex-col items-center mb-10 py-10">
+        <section className="mx-7 md:mx-14 border border-[#999999ff] rounded-[8rem] relative flex flex-col items-center mb-10 py-10">
           <div className="inline-flex items-center gap-1 bg-white border border-[#999999ff] rounded-[8rem] px-4 py-1 absolute top-[-20rem]">
             <img src={arrowBlue} alt="arrow blue" className="w-[12rem]" />
             <h2 className="text-lg font-bold">
@@ -137,7 +157,7 @@ const ResultPage = () => {
             </h2>
           </div>
 
-          <section className="flex gap-4">
+          <section className="flex gap-4 flex-wrap justify-center">
             <div>
               <p className="text-xl text-[#666666ff] font-medium ml-4">
                 Entrada:
@@ -148,7 +168,12 @@ const ResultPage = () => {
                   alt="arrow gray"
                   className="w-[10rem] h-[12rem]"
                 />
-                <p className="text-[#063958ff] font-bold text-xl">{`${Number(valorMesRetroativo).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}`}</p>
+                <p className="text-[#063958ff] font-bold text-xl">{`${Number(
+                  valorMesRetroativo
+                ).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}`}</p>
               </span>
             </div>
             <img
@@ -237,7 +262,7 @@ const ResultPage = () => {
           </section>
         </section>
 
-        <section className="mx-14 border border-[#999999ff] rounded-[8rem] relative flex justify-center items-center text-center mb-10 py-10 gap-8">
+        <section className="mx-7 md:mx-14 border border-[#999999ff] rounded-[8rem] relative flex justify-center items-center text-center mb-10 py-10 gap-8 flex-wrap">
           <div className="inline-flex items-center gap-1 bg-white border border-[#999999ff] rounded-[8rem] px-4 py-1 absolute top-[-20rem]">
             <img src={arrowBlue} alt="arrow blue" className="w-[12rem]" />
             <h2 className="text-lg font-bold">
@@ -246,7 +271,7 @@ const ResultPage = () => {
           </div>
 
           <div>
-            <p className="text-xl text-[#666666ff] font-medium ml-4">
+            <p className="text-xl text-[#666666ff] font-medium ml-4 mt-8 md:mt-0">
               Honorários:
             </p>
             <span className="inline-flex items-center gap-1 bg-[#eef1f6ff] px-4 py-2 rounded-[8rem]">
